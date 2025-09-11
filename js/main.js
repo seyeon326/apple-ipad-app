4b1b90e0655c68d00c762f5225f0911ea236c52d
seyeon326 <lseyeon0723@gmail.com>
2025-09-11 18:57:05 +0900

    헤더 검색 바 스크립트

--- js/main.js
+++ js/main.js
@@ -25,3 +25,24 @@ basketEl.addEventListener('click', function (event) {
 window.addEventListener('click', function () {
   hideBasket()
 });
+
+
+// 헤더 검색
+const headerEl = document.querySelector('header');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl  = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl  = searchWrapEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+    headerEl.classList.add('searching')
+    document.documentElement.classList.add('fixed');
+}
+function hideSearch() {
+    headerEl.classList.remove('searching');
+    document.documentElement.classList.remove('fixed');
+}
