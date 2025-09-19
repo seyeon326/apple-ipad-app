aed7d9db5e9b3a43d38e51763d1f0ad9093f2ded
seyeon326 <lseyeon0723@gmail.com>
2025-09-19 22:49:23 +0900

    Compare 섹션

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from '../data/ipads.js';
+
 // 헤더 장바구니 드롭다운
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
@@ -84,7 +86,7 @@ for (let i = 0; i < 60; i += 1) {
   x -= 100;
 }
 
-console.log(frames);
+// console.log(frames);
 
 
 // 요소의 가시성 관찰
@@ -116,4 +118,33 @@ pauseBtn.addEventListener('click', function () {
   video.pause();
   pauseBtn.classList.add('hide');
   playBtn.classList.remove('hide');
+})
+
+
+// '당신에게 맞는 iPad는?' 렌더링
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+
+  itemEl.innerHTML = /* html */`
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">&#8361;${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+
+  itemsEl.append(itemEl)
 })
